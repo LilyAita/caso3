da0b219f310ec44bd7cbd6a1d905c633aef3df29
LilyAita <la.duque@uniandes.edu.co>
2019-11-16 11:31:20 -0500

    ini

--- caso 3 infracom/src/main/MainClienteConSeguridad.java
+++ caso 3 infracom/src/main/MainClienteConSeguridad.java
@@ -9,7 +9,7 @@ public class MainClienteConSeguridad {
 	public static void main(String[] args) throws Exception {
 		
 		ClienteConSeguridad  c = new ClienteConSeguridad();
-		c = new ClienteConSeguridad();
+		
 	}
 
 }
